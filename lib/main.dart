e5b92740cbc0c155ee3c615e757d93ef98df5d09
Prasanna BMRD <bmruvinda2001@gmail.com>
2025-06-22 13:07:55 +0530

    Refactor Supabase initialization and improve error handling in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,45 +2,53 @@ import 'package:agro_zone/models/dbdata.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-const supabaseUrl = 'https://iddmbqmmwnglamblsyjt.supabase.co';
-const supabaseKey = String.fromEnvironment(
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkZG1icW1td25nbGFtYmxzeWp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NDE1NTgsImV4cCI6MjA2NjAxNzU1OH0.VPPFor_D3fS8cvibkj8Uqs5OilL9XIfBNrW5i8sfsGw',
-);
+// const supabaseUrl = 'https://iddmbqmmwnglamblsyjt.supabase.co';
+// const supabaseKey = String.fromEnvironment(
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkZG1icW1td25nbGFtYmxzeWp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NDE1NTgsImV4cCI6MjA2NjAxNzU1OH0.VPPFor_D3fS8cvibkj8Uqs5OilL9XIfBNrW5i8sfsGw',
+// );
 
 Future<void> main() async {
-  try {
-    await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
-    print('connected to supabase');
-  } catch (e) {
-    print('error connecting to supabase');
-  }
-
+  await Supabase.initialize(
+    url: 'https://iddmbqmmwnglamblsyjt.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkZG1icW1td25nbGFtYmxzeWp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NDE1NTgsImV4cCI6MjA2NjAxNzU1OH0.VPPFor_D3fS8cvibkj8Uqs5OilL9XIfBNrW5i8sfsGw',
+  );
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _future = Supabase.instance.client.from('user_plots_new').select();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: FutureBuilder(
-          future: SupabaseDataBaseData().getName(),
+          future: _future,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(child: CircularProgressIndicator());
             }
             if (snapshot.hasError) {
-              return Center(child: Text('Error'));
+              return Center(child: Text('Error: ${snapshot.error}'));
             }
-            final data = snapshot.data as List<dynamic>;
+            if (!snapshot.hasData || (snapshot.data as List).isEmpty) {
+              return const Center(child: Text('No data found.'));
+            }
+            final data = snapshot.data as List;
             return ListView.builder(
               itemCount: data.length,
-              itemBuilder: (context, index) {
-                final name = data[index]['name'];
-                return ListTile(title: Text(name));
-              },
+              itemBuilder: ((context, index) {
+                final name = data[index];
+                return ListTile(title: Text(name['name'].toString()));
+              }),
             );
           },
         ),
